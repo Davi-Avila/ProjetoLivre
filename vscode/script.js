9ac3a60f3a97bb934e5c9995e0bad2e0b26372ed
Davi-Avila <davidomingosavila202@gmail.com>
2025-11-25 18:17:53 -0300

    Delete vscode directory

--- vscode/script.js
+++
@@ -1,9 +0,0 @@
-function sumir(event, opcao) {
-    event.preventDefault(); // evita trocar de página
-    opcao.style.transition = "opacity 0.4s";
-    opcao.style.opacity = "0";
-
-    setTimeout(() => {
-        opcao.style.display = "none";
-    }, 400);
-}
